bdefbc7dfce9226201868e78fd1e79d399886c4d
guilleyn <g@guille.im>
2015-08-09 21:51:51 -0400

    comentario

---
+++ ejerFarey.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+
+ int main(){
+    int n,numerador=0,denominador=1,aux1=1,aux2=0,aux3,temp;
+    printf("Introduzca un numero: ");
+    scanf("%d",&n);
+    do{
+        printf("\n%d/%d" ,numerador,denominador);
+        aux3=(n-aux2)/denominador;
+        aux2+=(aux3*denominador);
+        aux1+=(aux3*numerador);
+        /* Intercambio de variables entre las auxiliares con el numerador y denominador respectivamente*/
+        temp=aux1;aux1=numerador;numerador=temp;
+        temp=aux2;aux2=denominador;denominador=temp;
+        /* Fin intercambio*/
+        aux1=-aux1;
+        aux2=-aux2;
+    }while(denominador>1);
+   printf("\n1/1");
+ }
